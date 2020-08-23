829fba790535afe8dcaab20f419ccced0f5ad3d3
erics1996 <erics1996@yeah.net>
2020-08-24 00:28:11 +0800

    webpack template...

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png"><br>
+    <router-link :to="{name:'Home'}">首页</router-link>
+    <router-link :to="{name:'Course',query:{userid:1}}">免费课程</router-link>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
